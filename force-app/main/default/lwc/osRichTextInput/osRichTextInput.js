4fd6f2fabec140f31c964f6419e706127f6c4ada
Dalil Djidel <dalil.djidel@hotmail.com>
2023-09-03 13:43:24 +0200

    Added header in .js

--- force-app/main/default/lwc/osRichTextInput/osRichTextInput.js
+++ force-app/main/default/lwc/osRichTextInput/osRichTextInput.js
@@ -1,3 +1,19 @@
+/**
+ * Custom Rich Text LWC to be integrated to OmniScript
+ *
+ * @author  Dalil Djidel (dalil.djidel@salesforce.com)
+ * @version 1.0
+ *
+ * History
+ * -------
+ * v1.0 - 11-18-2022 - Initial Version
+ * 
+ * License
+ * -------
+ * Creative Commons Zero v1.0 Universal
+ * 
+ */
+
 import { LightningElement } from 'lwc';
 import { OmniscriptBaseMixin } from 'omnistudio/omniscriptBaseMixin';
 
